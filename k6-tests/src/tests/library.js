10d77cc500e33a4ae82021fdd13a0e659c0e6523
Łukasz Latusik <latusikl@gmail.com>
2024-02-04 15:53:28 +0100

    Move path building logic to setup method

--- k6-tests/src/tests/library.js
+++ k6-tests/src/tests/library.js
@@ -32,74 +32,78 @@ export const options = {
     // },
 }
 
-const ITEMS_PATH = `${getBaseUrl()}${ApiPaths.ITEMS}`
-const STOCKS_PATH = `${getBaseUrl()}${ApiPaths.ITEM_STOCK}`
-const MEMBERS_PATH = `${getBaseUrl()}${ApiPaths.MEMBERS}`
-const BORROWINGS_PATH = `${getBaseUrl()}${ApiPaths.BORROWINGS}`
 
-export default function () {
+export function setup(){
+    return {
+        itemsPath: `${getBaseUrl()}${ApiPaths.ITEMS}`,
+        stocksPath: `${getBaseUrl()}${ApiPaths.ITEM_STOCK}`,
+        membersPath: `${getBaseUrl()}${ApiPaths.MEMBERS}`,
+        borrowingsPath: `${getBaseUrl()}${ApiPaths.BORROWINGS}`
+    }
+}
+export default function (setupData) {
 
     console.debug("STEP 1: Create book");
-    const createBookResponse = http.post(ITEMS_PATH, randomisedItemDto(), DEFAULT_PARAMS)
+    const createBookResponse = http.post(setupData.itemsPath, randomisedItemDto(), DEFAULT_PARAMS)
     checkPostResponse(createBookResponse)
     const bookId = extractUUIDFromResponseHeader(createBookResponse)
     // sleepBetween1to3sec()
 
     console.debug("STEP 2: Creat stock for book")
-    const createStockResponse = http.post(STOCKS_PATH, bookStockDto(bookId), DEFAULT_PARAMS)
+    const createStockResponse = http.post(setupData.stocksPath, bookStockDto(bookId), DEFAULT_PARAMS)
     checkPostResponse(createStockResponse)
     const stockId = extractUUIDFromResponseHeader(createStockResponse)
     // sleepBetween1to3sec()
 
     console.debug("STEP 3: Create new library member")
-    const createMemberResponse = http.post(MEMBERS_PATH, randomisedMemberDto(), DEFAULT_PARAMS)
+    const createMemberResponse = http.post(setupData.membersPath, randomisedMemberDto(), DEFAULT_PARAMS)
     checkPostResponse(createMemberResponse)
     const memberId = extractUUIDFromResponseHeader(createMemberResponse)
     // sleepBetween1to3sec()
 
     console.debug("STEP 4: Check book stock")
-    const bookStockResponse = http.get(`${STOCKS_PATH}/${stockId}`, DEFAULT_PARAMS)
+    const bookStockResponse = http.get(`${setupData.stocksPath}/${stockId}`, DEFAULT_PARAMS)
     checkBookStock(bookStockResponse,2,0)
     // sleepBetween1to3sec()
 
     console.debug("STEP 5: Borrow book")
-    const borrowingResponse = http.post(BORROWINGS_PATH,borrowDto(memberId,bookId),DEFAULT_PARAMS)
+    const borrowingResponse = http.post(setupData.borrowingsPath,borrowDto(memberId,bookId),DEFAULT_PARAMS)
     checkPostResponse(borrowingResponse)
     const borrowingId = extractUUIDFromResponseHeader(borrowingResponse)
     // sleepBetween1to3sec()
 
     console.debug("STEP 6: Check book stock")
-    const bookStockResponseAfterBorrow = http.get(`${STOCKS_PATH}/${stockId}`, DEFAULT_PARAMS)
+    const bookStockResponseAfterBorrow = http.get(`${setupData.stocksPath}/${stockId}`, DEFAULT_PARAMS)
     checkBookStock(bookStockResponseAfterBorrow,2,1)
     // sleepBetween1to3sec()
 
     console.debug("STEP 7: Return Book")
-    const borrowingRemovalResponse = http.del(`${BORROWINGS_PATH}/${borrowingId}`,null,DEFAULT_PARAMS)
+    const borrowingRemovalResponse = http.del(`${setupData.borrowingsPath}/${borrowingId}`,null,DEFAULT_PARAMS)
     checkDeleteResponse(borrowingRemovalResponse)
     // sleepBetween1to3sec()
 
     console.debug("STEP 8: Check book stock")
-    const bookStockResponseAfterReturn = http.get(`${STOCKS_PATH}/${stockId}`, DEFAULT_PARAMS)
+    const bookStockResponseAfterReturn = http.get(`${setupData.stocksPath}/${stockId}`, DEFAULT_PARAMS)
     checkBookStock(bookStockResponseAfterReturn,2,0)
     // sleepBetween1to3sec()
 
     console.debug("STEP 9: Remove book stock")
-    const bookStockRemovalResponse = http.del(`${BORROWINGS_PATH}/${borrowingId}`,null,DEFAULT_PARAMS)
+    const bookStockRemovalResponse = http.del(`${setupData.borrowingsPath}/${borrowingId}`,null,DEFAULT_PARAMS)
     checkDeleteResponse(bookStockRemovalResponse)
     // sleepBetween1to3sec()
 
     console.debug("STEP 10: Remove book")
-    const bookRemovalResponse = http.del(`${ITEMS_PATH}/${bookId}`,null,DEFAULT_PARAMS)
+    const bookRemovalResponse = http.del(`${setupData.itemsPath}/${bookId}`,null,DEFAULT_PARAMS)
     checkDeleteResponse(bookRemovalResponse)
     // sleepBetween1to3sec()
 
     console.debug("STEP 11: Remove Member")
-    const memberRemovalResponse = http.del(`${MEMBERS_PATH}/${memberId}`,null, DEFAULT_PARAMS)
+    const memberRemovalResponse = http.del(`${setupData.membersPath}/${memberId}`,null, DEFAULT_PARAMS)
     checkDeleteResponse(memberRemovalResponse)
     // sleepBetween1to3sec()
 
     console.debug("STEP 12: Check member does exist")
-    const memberFetchResponse = http.get(`${MEMBERS_PATH}/${memberId}`,DEFAULT_PARAMS)
+    const memberFetchResponse = http.get(`${setupData.membersPath}/${memberId}`,DEFAULT_PARAMS)
     check(memberFetchResponse,{
         'Has status 404': res => res.status === HttpStatus.NOT_FOUND
     })
